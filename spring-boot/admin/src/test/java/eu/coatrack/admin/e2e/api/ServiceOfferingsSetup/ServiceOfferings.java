6d82cfc45b62eaf5d24ec1230911d0ed1cea0934
Christoph Baier <cbaier@uni-bremen.de>
2021-08-25 17:03:27 +0200

    Service deletion is now working.

--- spring-boot/admin/src/test/java/eu/coatrack/admin/e2e/api/ServiceOfferingsSetup/ServiceOfferings.java
+++ spring-boot/admin/src/test/java/eu/coatrack/admin/e2e/api/ServiceOfferingsSetup/ServiceOfferings.java
@@ -8,7 +8,6 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.Random;
 import java.util.stream.Collectors;
 
@@ -57,12 +56,9 @@ public class ServiceOfferings {
 
     public boolean isServiceWithinList(String serviceName) {
         driver.get(pathPrefix + "/admin/services");
-        waitForElementWithId("servicesTable");
-        WebElement servicesTable = driver.findElement(By.id("servicesTable"));
-        List<WebElement> rows = servicesTable.findElements(By.cssSelector("tr"));
-        rows.remove(0);
+        List<WebElement> rows = getServicesRows();
         List<String> listOfServiceNames = rows.stream().map(row -> row.findElement(By.cssSelector("td")).getText()).collect(Collectors.toList());
-
+        driver.navigate().refresh();
         return listOfServiceNames.contains(serviceName);
     }
 
@@ -70,4 +66,31 @@ public class ServiceOfferings {
         new WebDriverWait(driver, 3).until(ExpectedConditions.presenceOfElementLocated(By.id(id)));
     }
 
+    public void deleteService(String serviceName) {
+        driver.get(pathPrefix + "/admin/services");
+
+        List<WebElement> rows = getServicesRows();
+        WebElement rowOfDesiredService = rows.stream().filter(row -> row.findElement(By.cssSelector("td")).getText().equals(serviceName)).findFirst().get();
+        List<WebElement> listOfRowElements = rowOfDesiredService.findElements(By.cssSelector("td"));
+
+        WebElement cellWithTrashButton = listOfRowElements.get(6);
+        WebElement trashButton = cellWithTrashButton.findElements(By.cssSelector("i")).get(3);
+        trashButton.click();
+
+        try {
+            Thread.sleep(2000);
+        } catch (Exception ignored) {}
+        WebElement infoDialog = driver.findElement(By.className("ui-dialog"));
+        List<WebElement> listOfButtons = infoDialog.findElements(By.cssSelector("button"));
+        WebElement yesButton = listOfButtons.stream().filter(button -> button.getText().contains("Yes")).findFirst().get();
+        yesButton.click();
+    }
+
+    private List<WebElement> getServicesRows() {
+        waitForElementWithId("servicesTable");
+        WebElement servicesTable = driver.findElement(By.id("servicesTable"));
+        List<WebElement> rows = servicesTable.findElements(By.cssSelector("tr"));
+        rows.remove(0);
+        return rows;
+    }
 }
--- spring-boot/admin/src/test/java/eu/coatrack/admin/e2e/tests/CreationTests.java
+++ spring-boot/admin/src/test/java/eu/coatrack/admin/e2e/tests/CreationTests.java
@@ -4,6 +4,7 @@ import eu.coatrack.admin.e2e.AbstractTestSetup;
 import eu.coatrack.admin.e2e.api.ServiceOfferingsSetup.ServiceOfferings;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CreationTests extends AbstractTestSetup {
@@ -12,11 +13,10 @@ public class CreationTests extends AbstractTestSetup {
     public void createServiceTest() {
         ServiceOfferings serviceOfferings = pageProvider.getServiceOfferings();
         String serviceName = serviceOfferings.createService();
-
         assertTrue(serviceOfferings.isServiceWithinList(serviceName));
 
-        //deleteService(serviceName) -> void
-        //isServiceWithinList(serviceName) -> assertFalse
+        serviceOfferings.deleteService(serviceName);
+        assertFalse(serviceOfferings.isServiceWithinList(serviceName));
     }
 
     @Test

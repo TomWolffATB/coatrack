037ce47a086b60046f5899a782b5b68edbaac71d
Christoph Baier <cbaier@uni-bremen.de>
2021-09-21 09:18:36 +0200

    Refactored sleepMillis method.

--- spring-boot/admin/src/test/java/eu/coatrack/admin/e2e/api/tools/WaiterUtils.java
+++ spring-boot/admin/src/test/java/eu/coatrack/admin/e2e/api/tools/WaiterUtils.java
@@ -4,17 +4,28 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class WaiterUtils {
 
-    public static void waitForElementWithId(String id, WebDriver driver) {
+    private final WebDriver driver;
+    private static final Logger logger = LoggerFactory.getLogger(WaiterUtils.class);
+
+    public WaiterUtils(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void waitForElementWithId(String id) {
         new WebDriverWait(driver, 3).until(ExpectedConditions.elementToBeClickable(By.id(id)));
     }
 
     public static void sleepMillis(int millis) {
         try {
             Thread.sleep(millis);
-        } catch (Exception ignored){}
+        } catch (Exception e){
+            logger.error("The sleep process failed.", e);
+        }
     }
 
 }

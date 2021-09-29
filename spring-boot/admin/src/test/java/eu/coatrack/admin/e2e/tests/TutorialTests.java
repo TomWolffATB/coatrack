6b6025cf4d9ae02213cee310ab34da97c018252c
Christoph Baier <cbaier@uni-bremen.de>
2021-09-29 10:14:02 +0200

    Transferred jar execution logic to tutorial test.

--- spring-boot/admin/src/test/java/eu/coatrack/admin/e2e/tests/TutorialTests.java
+++ spring-boot/admin/src/test/java/eu/coatrack/admin/e2e/tests/TutorialTests.java
@@ -2,7 +2,12 @@ package eu.coatrack.admin.e2e.tests;
 
 import eu.coatrack.admin.e2e.api.serviceProvider.ItemDto;
 import eu.coatrack.admin.e2e.api.serviceProvider.serviceOfferingsSetup.AdminServiceGateways;
+import org.apache.commons.exec.CommandLine;
+import org.apache.commons.exec.DefaultExecutor;
+import org.apache.commons.exec.ExecuteWatchdog;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.*;
 
@@ -11,6 +16,8 @@ import static eu.coatrack.admin.e2e.configuration.PageConfiguration.host;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TutorialTests extends AbstractTestSetup {
+
+    private static final Logger logger = LoggerFactory.getLogger(TutorialTests.class);
 
     @Test
     public void tutorialTest() throws IOException, InterruptedException {
@@ -23,7 +30,23 @@ public class TutorialTests extends AbstractTestSetup {
         assertFalse(file.exists());
 
         //TODO Gateway download test should be an isolated test and therefore separated from tutorial test.
+        // I want to test the download feature and only if this works, then continue to test jar execution features using other tests?
         //TODO Gateway download should be hidden behind the API.
+        downloadGateway(itemDto);
+
+        assertTrue(file.exists());
+        assertTrue(file.length() > 1000);
+
+        Thread jarThread = executeJar();
+        Thread.sleep(30000);
+        jarThread.interrupt();
+
+        //TODO Extend the API to make calls by
+
+        cleanup(itemDto, file);
+    }
+
+    private void downloadGateway(ItemDto itemDto) throws IOException, InterruptedException {
         Runtime rt = Runtime.getRuntime();
         String firstPartOfCommand = "cmd /c curl -v ";
         if (host.equals("localhost"))
@@ -31,44 +54,35 @@ public class TutorialTests extends AbstractTestSetup {
         String command = firstPartOfCommand + "--cookie \"SESSION=" + sessionCookie.getValue() + "\" --output ./test.jar " + itemDto.gatewayDownloadLink;
         Process pr = rt.exec(command);
         pr.waitFor();
+    }
 
-        assertTrue(file.exists());
-        assertTrue(file.length() > 1000);
-        file.delete();
-
-        /*
-        //Run gateway jar
-        Process pr2 = rt.exec("cmd /c start cmd /k java -jar ./test.jar");
-        // Approaches to get this working:
-         1) pr2.send("STRG + C")
-         2) Start commandline window -> after that send the second command to that process
-         3) rt.exec("java -jar ./test.jar");
-
-
-        sleepMillis(30000);
-
-        //This is somehow not sufficient. This could help: https://stackoverflow.com/questions/19726804/how-to-kill-a-process-in-java-process-destroy
-        pr2.destroyForcibly();
-
-        Process pr3 = rt.exec("cmd /c taskkill /F /PID " + pr2.pid());
-
-        BufferedReader reader2 = new BufferedReader(new InputStreamReader(pr3.getInputStream()));
-
-        String line2 = reader2.readLine();
-        while (line2 != null){
-            System.out.println(line2);
-            line2 = reader2.readLine();
-        }
-        */
-
-        //Test if API key reaches example website
-
+    private void cleanup(ItemDto itemDto, File file) {
         AdminServiceGateways adminServiceGateways = pageFactory.getServiceGateways();
         String gatewayName = adminServiceGateways.getGatewayNameByIdentifier(itemDto.gatewayIdentifier);
         adminServiceGateways.deleteGateway(gatewayName);
 
         pageFactory.getApiKeys().deleteApiKey(itemDto.apiKeyValue);
         pageFactory.getServiceOfferings().deleteService(itemDto.serviceName);
+
+        file.delete();
+        assertFalse(file.exists());
+    }
+
+    private Thread executeJar() {
+        Thread jarExecutionThread = new Thread(() -> {
+            String line = "java -jar ./test.jar";
+            CommandLine cmdLine = CommandLine.parse(line);
+            DefaultExecutor executor = new DefaultExecutor();
+            ExecuteWatchdog watchdog = new ExecuteWatchdog(120000);
+            executor.setWatchdog(watchdog);
+            try {
+                executor.execute(cmdLine);
+            } catch (IOException e) {
+                logger.info("The execution of the jar file was interrupted.");
+            }
+        });
+        jarExecutionThread.start();
+        return jarExecutionThread;
     }
 
 }
